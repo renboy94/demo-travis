8afa3244ae14f8f6b011328f61f11c25fb88df86
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-19 19:40:20 +0000

    Deploy renboy94/demo-travis to github.com/renboy94/demo-travis.git:gh-pages

---
+++ precache-manifest.2044a649b4c32c40e804db8d65b34d73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e61d3392b81df7ca85e501c18670fd7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71b3e4406ea30f169335",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "31e159844cd3971be63c",
+    "url": "/static/js/2.14e5e52e.chunk.js"
+  },
+  {
+    "revision": "71b3e4406ea30f169335",
+    "url": "/static/js/main.36262146.chunk.js"
+  },
+  {
+    "revision": "9d0befe3701599abf127",
+    "url": "/static/js/runtime-main.da1a12be.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
